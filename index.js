6eae2f93037565ca36f673b246f2f126c498730a
good morning <172862789+jr-jpg@users.noreply.github.com>
2024-09-09 14:42:56 -0500

    Debugged the functions to render the shape and write the svg to a file

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const inquirer = require('inquirer');
 const questions = require('./lib/questions.js');
-const writeToFile = require('./lib/writeToFile.js');
+// const writeToFile = require('./lib/writeToFile.js');
+
+// Use the file system to write files
+const fs = require('fs');
+
+// Import the module to render the svg
+const createShape = require('./lib/createShape.js');
+
+// A function to use the data collected to create the SVG file
+function writeToFile(answers) {
+    const fileName = `./examples/logo.svg`;
+
+    const svg = createShape(answers);
+
+    fs.writeFile(fileName, svg, (err) =>
+        err ? console.log(err) : console.log('Generated logo.svg'))
+}
 
 function init() {
     return inquirer
@@ -9,6 +25,7 @@ function init() {
     
 init() 
     .then((answers) => {
+        console.log(answers);
         writeToFile(answers);
     })
      .catch((err) => {
